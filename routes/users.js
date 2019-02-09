02309e7895bcfd5ca0193d5b7838cc07382e03e6
Miguelerja <miguelerja@gmail.com>
2019-02-09 17:32:25 +0100

    Add profile page and search logic

--- routes/users.js
+++ routes/users.js
@@ -17,4 +17,36 @@ router.get('/profile', (req, res, next) => {
     .catch(next);
 });
 
+/* POST Add friend page */
+
+router.post('/profile/search', (req, res, next) => {
+  const searchField = req.body.search;
+  User.find({"username" : searchField})
+    .then((user) => {
+      res.render('search', { user });
+    })
+    .catch(next);
+});
+
+/* POST Add friend to friend list */
+
+router.post('/profile', (req, res, next) => {
+  const friendId = req.body.id;
+  const userId = req.session.currentUser._id;
+
+  User.findById(userId)
+    .then((user) => {
+      const updatedFriends = user.myFriends;
+      updatedFriends.push(friendId);
+
+      User.findByIdAndUpdate(userId, { 'myfriends': updatedFriends })
+        .then((user) => {
+          console.log(user)
+          res.redirect('profile');
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
+
 module.exports = router;
