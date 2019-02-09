489dcaa968585e8dbc7aca7ea4972aae24bf558e
Miguelerja <miguelerja@gmail.com>
2019-02-09 10:57:04 +0100

    Add logic to section 1 and 2 of the bill creator

--- routes/bills.js
+++ routes/bills.js
@@ -6,12 +6,58 @@ const Bill = require('../models/bill');
 
 /* GET main page */
 
+/* GET items bill */
 
+router.get('/items', (req, res, next) => {
+  res.render('bills/items');
+});
 
-/* GET new bill */
+/* POST items bill */
 
-router.get('/new', (req, res, next) => {
-  res.render('bills/new');
+router.post('/', (req, res, next) => {
+  const creator = req.session.currentUser._id;
+  const names = req.body.name;
+  const prices = req.body.price;
+  const billItems = [];
+
+  names.forEach((name, index) => {
+    const item = {
+      name: names[index],
+      price: prices[index],
+    };
+    billItems.push(item);
+  });
+
+  const bill = {
+    creatorId: creator,
+    items: billItems,
+  };
+
+  User.findById(creator)
+    .populate('myFriends')
+    .then((friends) => {
+      const friendsNames = friends.myFriends;
+
+      Bill.create(bill)
+        .then((bill) => {
+          req.session.newBill = bill;
+          res.render('bills/participants', { friendsNames });
+        })
+        .catch(next);
+    })
+    .catch(next);
+});
+
+router.post('/participants', (req,res,next) => {
+  const participants = Object.keys(req.body);
+  const billId = req.session.newBill._id;
+  participants.forEach((participant) => {
+    User.findOne({ username: participant })
+      .then((participant) => {
+        console.log(participant);
+      })
+      .catch(next);
+  });
 });
 
 /* GET main bill */
@@ -24,35 +70,5 @@ router.get('/:id', (req, res, next) => {
     }).catch(next);
 });
 
-/* POST new bill */
-
-router.post('/edit', (req, res, next) => {
-  const creator = req.session.currentUser._id;
-  const names = req.body.name;
-  const prices = req.body.price;
-  console.log(req.session);
-  const billItems = [];
-  names.forEach((name, index) => {
-    const item = {
-      name: names[index],
-      price: prices[index],
-    }
-    billItems.push(item);
-  });
-  console.log(billItems);
-  const bill = {
-    creatorId: creator,
-    items: billItems,
-  }
-  Bill.create(bill)
-    .then(() => {
-      res.render('bills/editBill');
-    })
-    .catch(next);
-});
-
-router.post('/', (req, res, next) => {
-  console.log(req.body);
-})
 
 module.exports = router;
