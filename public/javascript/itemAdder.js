f01c76555cac054d409a8b6f168e11bb6715cbb2
Miguelerja <miguelerja@gmail.com>
2019-02-10 12:30:33 +0100

    Add decimal numbers for prices

--- public/javascript/itemAdder.js
+++ public/javascript/itemAdder.js
@@ -1,20 +1,12 @@
-const buttons = document.querySelectorAll('.item-adder');
-const billInformation = document.getElementById('billInformation');
-const names = document.querySelectorAll('.name-field');
-const prices = document.querySelectorAll('.price-field');
-const html = `<div class="item-input">
-<input type="text" name="name" placeholder="Item">
-<input type="number" name="price" placeholder="€">
-</div>`;
+const itemAdder = document.getElementById('item-adder');
+const container = document.getElementById('items-list-container');
 
-const billContents = {};
+const html = `<input type="text" name="name" Placeholder="Item"> 
+<input type="number" step="0.01" name="price" Placeholder="€">`;
 
-buttons.forEach((button, index) => {
-  const userName = button.getAttribute('id');
-  billContents[userName] = [];
-  button.addEventListener('click', () => {
-    const inputs = button.previousElementSibling;
-    console.log(inputs.querySelectorAll('input'));
-    button.previousElementSibling.insertAdjacentHTML('afterend', html);
-  });
+itemAdder.addEventListener('click', () => {
+  const div = document.createElement('div');
+  div.setAttribute('class', 'item-container');
+  div.innerHTML = html;
+  container.appendChild(div);
 });
