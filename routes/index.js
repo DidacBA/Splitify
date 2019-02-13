8af667e47a558c909d82f90008f98f3af069f487
Didac Bigorda <didacbigorda@gmail.com>
2019-02-13 16:27:30 +0100

    Add bill details to main page

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const User = require('../models/user');
+const Bill = require('../models/bill');
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('main');
+  const session = req.session.currentUser;
+  Bill.find({ creatorId: session._id }).sort({ date: -1 }).limit(2)
+    .then((bills) => {
+      const bill1Date = new Date(bills[0].createdAt).toDateString();
+      const bill2Date = new Date(bills[1].createdAt).toDateString();
+      res.render('main', { bills, bill1Date, bill2Date });
+    })
+    .catch(next);
 });
 
 module.exports = router;
