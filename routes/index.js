8f60f0ca8fcfda9d885c7f53048c1792d2bdf0ff
Didac Bigorda <didacbigorda@gmail.com>
2019-02-13 19:08:24 +0100

    Add views for no bills and one bill

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const User = require('../models/user');
 const Bill = require('../models/bill');
+const isEmpty = require('../helpers/helpers');
 
 const router = express.Router();
 
 /* GET home page. */
+
 router.get('/', (req, res, next) => {
   const session = req.session.currentUser;
   Bill.find({ creatorId: session._id }).sort({ date: -1 }).limit(2)
     .then((bills) => {
-      const bill1Date = new Date(bills[0].createdAt).toDateString();
-      const bill2Date = new Date(bills[1].createdAt).toDateString();
-      res.render('main', { bills, bill1Date, bill2Date });
+      if (isEmpty(bills)) {
+        res.render('main-empty');
+        return;
+      }
+      if (bills.length === 1) {
+        const dates = [
+          new Date(bills[0].createdAt).toDateString(),
+        ];
+        res.render('main-one-bill', { bills, dates });
+      } else {
+        const dates = [
+          new Date(bills[0].createdAt).toDateString(),
+          new Date(bills[1].createdAt).toDateString(),
+        ];
+        res.render('main', { bills, dates });
+      }
     })
     .catch(next);
 });
