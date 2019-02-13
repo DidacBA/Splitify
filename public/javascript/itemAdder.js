6084b365c5fc06ca9db4e24c313b72a00b8633c9
Miguelerja <miguelerja@gmail.com>
2019-02-13 15:52:26 +0100

    Add css on profile and search

--- public/javascript/itemAdder.js
+++ public/javascript/itemAdder.js
@@ -2,8 +2,8 @@ function itemAdder() {
   const itemAdder = document.getElementById('item-adder');
   const container = document.getElementById('items-list-container');
 
-  const html = `<input class="item-name item-input item" type="text" name="name" Placeholder="Item"> 
-  <input class="item-price item-input" type="number" step="0.01" name="price" Placeholder="€">`;
+  const html = `<input class="item-name item-input item" type="text" name="name" Placeholder="Item" required> 
+  <input class="item-price item-input" type="number" step="0.01" name="price" Placeholder="€" required>`;
 
   itemAdder.addEventListener('click', () => {
     const div = document.createElement('div');
---
+++ public/javascript/profile.js
@@ -0,0 +1,4 @@
+const username = document.getElementsByClassName('username');
+const email = document.getElementsByClassName('email');
+const joinDate = document.getElementsByClassName('date');
+
