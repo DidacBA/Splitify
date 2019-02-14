2731292e1d3f63197f43fb6bac25475a6e3c3e54
Miguelerja <miguelerja@gmail.com>
2019-02-14 10:49:23 +0100

    Fix bug in search

--- app.js
+++ app.js
@@ -16,13 +16,9 @@ const notifications = require('./middlewares/flash');
 // Set up mongoose and Mongo connection
 
 mongoose
-  .connect('mongodb://localhost:27017/splitify', { useNewUrlParser: true })
-  .then((x) => {
-    console.log(`Connected to Mongo! Database name: '${x.connections[0].name}'`);
-  })
-  .catch((err) => {
-    console.error('Error connecting to mongo', err);
-  });
+  .connect(process.env.DB_URL, { useNewUrlParser: true })
+  .then()
+  .catch();
 
 // Connect routers
 
@@ -40,7 +36,7 @@ app.use(session({
     mongooseConnection: mongoose.connection,
     ttl: 24 * 60 * 60, // 1 day
   }),
-  secret: 'splitify',
+  secret: process.env.SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: {
